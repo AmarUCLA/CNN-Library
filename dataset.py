f7bc2586aa2b1207bc25e02ce63bd8cb97992064
Amar <64054906+AmarUCLA@users.noreply.github.com>
2022-05-23 12:01:41 -0700

    Add files via upload

--- dataset.py
+++ dataset.py
@@ -1,40 +1,24 @@
 import torch
 import torchvision
-from augmentations import augmentation, ContrastiveAugmentation
-import torchvision.transforms as transforms
+from .QCDataset import QCDataset
+import glob
+
 
 class initialize_dataset:
-    def __init__(self, image_resolution=224, batch_size=128, MNIST=True):
+    def __init__(self, image_resolution=320, batch_size=128, MNIST=True):
         self.image_resolution= image_resolution
         self.batch_size=batch_size
-        self.MNIST = MNIST
-
+        self.MNIST=MNIST
+        self.trainDir = '/Users/ravinderkaur/Documents/PathologyPipeline/QCTraining/TrainingImages/*'
+        self.testDir = '/Users/ravinderkaur/Documents/PathologyPipeline/QCTraining/EvalImages/*'
+        self.labelDir = '/Users/ravinderkaur/Documents/PathologyPipeline/QCTraining/SlideData/'
+    
     def load_dataset(self, transform=False):
-        path = "/home/mayur/Desktop/Pytorch/data"
-        #path = './data'
-        if transform:
-            transform = augmentation(image_resolution=self.image_resolution)
-        elif self.MNIST:
-            transform = transforms.Compose([transforms.ToTensor(), transforms.Resize((self.image_resolution, self.image_resolution)),
-                                            transforms.Normalize((0.1307,), (0.3081,))])
-        else:
-            transform = transforms.Compose([transforms.ToTensor(), transforms.Resize((self.image_resolution, self.image_resolution)),
-                        transforms.RandomHorizontalFlip(), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+        trainList = glob.glob(self.trainDir)
+        testList = glob.glob(self.testDir)
 
-        if self.MNIST:
-            train_dataset = torchvision.datasets.MNIST(root=path, train=True,
-                                                        transform = transform,
-                                                        download=True)
-            test_dataset = torchvision.datasets.MNIST(root=path, train=False,
-                                                    transform = transform,
-                                                    download=True)
-        else:
-            train_dataset = torchvision.datasets.CIFAR10(root=path, train=True,
-                                                        transform = transform,
-                                                        download=True)
-            test_dataset = torchvision.datasets.CIFAR10(root=path, train=False,
-                                                    transform = transform,
-                                                    download=True)
+        train_dataset = QCDataset(trainList, self.labelDir)
+        test_dataset = QCDataset(testList, self.labelDir)
 
         train_dataloader = torch.utils.data.DataLoader(dataset = train_dataset,
                                                         batch_size=self.batch_size,
@@ -44,3 +28,5 @@ class initialize_dataset:
                                                         shuffle=True)
 
         return train_dataloader, test_dataloader
+
+
